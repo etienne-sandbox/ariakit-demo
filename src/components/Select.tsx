db3d2a67222e189e1d57df2905e6be4d6f23f786
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-10-02 14:41:34 +0200

    Implement Select component with state management and rendering logic

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,3 +1,10 @@
+import * as Ariakit from "@ariakit/react";
+import { CaretDown, CaretUp } from "@phosphor-icons/react";
+import { Button } from "./button/Button";
+import { ButtonContent } from "./button/ButtonContent";
+import { Paper } from "./Paper";
+import { SelectItem } from "./SelectItem";
+
 export interface TSelectItem {
   value: string;
   title: string;
@@ -11,5 +18,44 @@ interface SelectProps {
 }
 
 export function Select({ label, items }: SelectProps) {
-  return <div>TODO</div>;
+  const store = Ariakit.useSelectStore({ defaultValue: items[0].value });
+
+  const value = store.useState((s) => s.value);
+  const selectedItem = items.find((item) => item.value === value)!;
+  const open = store.useState((s) => s.open);
+
+  return (
+    <div>
+      <Ariakit.SelectProvider store={store}>
+        <Ariakit.SelectLabel>{label}</Ariakit.SelectLabel>
+        <Ariakit.Select render={<Button variant="primary" />}>
+          <ButtonContent
+            title={selectedItem.title}
+            icon={selectedItem.icon}
+            endIcon={open ? <CaretUp /> : <CaretDown />}
+          />
+        </Ariakit.Select>
+        <Ariakit.SelectPopover
+          gutter={4}
+          modal
+          overlap
+          render={
+            <Paper
+              level="popover"
+              className="p-1.5 flex flex-col min-w-[--popover-anchor-width] max-h-[--popover-available-height] overflow-y-auto scrollbar-hide"
+            />
+          }
+        >
+          {items.map((item) => (
+            <Ariakit.SelectItem
+              key={item.value}
+              value={item.value}
+              disabled={item.disabled}
+              render={<SelectItem item={item} />}
+            />
+          ))}
+        </Ariakit.SelectPopover>
+      </Ariakit.SelectProvider>
+    </div>
+  );
 }
